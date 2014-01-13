ab2a08c75cadaffbd3e5a927af98fb26e943f1b6
Gabriel Araujo <gabrielqa@me.com>
2014-01-13 05:21:21 -0200

    #ZUP-107 #done Modal de criar tag

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('zupPainelApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ui.bootstrap'
 ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/tags.js
+++ app/scripts/controllers/tags.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('zupPainelApp')
-  .controller('TagsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+.controller('TagsCtrl', function ($scope, $modal) {
+
+  $scope.open = function () {
+    $modal.open({
+      templateUrl: 'addTag.html',
+      windowClass: 'tagModal',
+      controller: function($scope, $modalInstance) {
+        $scope.ok = function () {
+          $modalInstance.close();
+        };
+      }
+    });
+  };
+
+});
