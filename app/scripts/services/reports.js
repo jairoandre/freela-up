c76b8b5692401cd7a9edc5c840c419f58ae0df89
Gabriel Araujo <gabrielqa@me.com>
2014-01-19 12:19:27 -0200

    adicionado action para pegar items por categoria

--- app/scripts/services/reports.js
+++ app/scripts/services/reports.js
@@ -5,6 +5,7 @@ angular.module('zupPainelApp')
 .factory('Reports', function ($resource) {
   return $resource('{base_url}/reports/:id.json', { id:'@id' },
     {
-      'getAllCategories': { action: '{base_url}/reports/categories.json', method: 'GET' }
+      'getAllCategories': { action: '{base_url}/reports/categories.json', method: 'GET' },
+      'getItemsByCategory': { action: '{base_url}/:categoryId/items.json', method: 'GET', params: { categoryId:'@categoryId' } },
     });
 });
