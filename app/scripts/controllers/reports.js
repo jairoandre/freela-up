df235c47e12fde4e0b6412bfc0d5d749a7296189
Gabriel Araujo <gabrielqa@me.com>
2014-01-19 12:40:42 -0200

    configurado controllers para relatos

--- app/scripts/controllers/reports.js
+++ app/scripts/controllers/reports.js
@@ -1,10 +1,28 @@
 'use strict';
 
 angular.module('zupPainelApp')
-  .controller('ReportsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+.controller('ReportsCtrl', function ($scope, Reports) {
+
+  $scope.loading = true;
+
+  // Get all reports categories
+  Reports.getAllCategories(function(data) {
+    $scope.categories = data.categories;
+
+    $scope.loading = false;
   });
+})
+
+.controller('ViewItemsReportsCtrl', function ($scope, Reports, $routeParams) {
+
+  $scope.loading = true;
+
+  // Get reports from a specific category
+  Reports.getItemsByCategory({ categoryId: $routeParams.categoryId }, function(data) {
+    $scope.reports = data.reports;
+
+    $scope.loading = false;
+  });
+
+});
