58d195c9d0a5a4938187c05c2fde3dbef8305a98
Gabriel Araujo <gabrielqa@me.com>
2014-01-25 11:29:01 -0200

    #ZUP-143 #done e #ZUP-145 #done

--- app/scripts/controllers/groups.js
+++ app/scripts/controllers/groups.js
@@ -6,12 +6,33 @@ angular.module('zupPainelApp')
 
   $scope.loading = true;
 
+  var params = {};
+
   // Get all groups
-  Groups.getAll(function(data) {
+  var groupsData = Groups.getAll(function(data) {
     $scope.groups = data.groups;
 
     $scope.loading = false;
   });
+
+  $scope.search = function(text) {
+    if (text === '')
+    {
+      delete params.name;
+    }
+    else
+    {
+      params.name = text;
+    }
+
+    $scope.loadingContent = true;
+
+    groupsData = Groups.getAll(params, function(data) {
+      $scope.groups = data.groups;
+
+      $scope.loadingContent = false;
+    });
+  };
 })
 
 .controller('ViewGroupsCtrl', function ($scope, Groups, $routeParams) {
@@ -19,8 +40,9 @@ angular.module('zupPainelApp')
   $scope.loading = true;
 
   // Get specific group
-  Groups.get({ id: $routeParams.id }, function(data) {
+  Groups.getUsers({ id: $routeParams.id }, function(data) {
     $scope.group = data.group;
+    $scope.users = data.users;
 
     $scope.loading = false;
   });
--- app/scripts/services/groups.js
+++ app/scripts/services/groups.js
@@ -6,6 +6,7 @@ angular.module('zupPainelApp')
   return $resource('{base_url}/groups/:id.json', { id:'@id' },
     {
       'update': { method: 'PUT' },
-      'getAll': { method: 'GET' }
+      'getAll': { method: 'GET' },
+      'getUsers': { url: '{base_url}/groups/:id/users.json', method: 'GET', params: { id:'@id' } },
     });
 });
