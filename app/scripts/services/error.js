cb3580fe1183d82def79cd6c47be3d1cbddeb9df
Gabriel Araujo <gabrielqa@me.com>
2014-01-28 23:47:16 -0200

    bug fix

--- app/scripts/services/error.js
+++ app/scripts/services/error.js
@@ -1,28 +1,29 @@
 'use strict';
 
 angular.module('zupPainelApp')
-  .factory('Error', function ($modal) {
 
-    return {
-      // Show a pretty modal with debug information about the error
-      showDetails: function (response) {
+.factory('Error', function ($modal) {
 
-        $modal.open({
-          templateUrl: 'views/modal_error.html',
-          resolve: {
-            response: function() {
-              return response;
-            }
-          },
-          controller: ['$scope', '$modalInstance', 'response', function($scope, $modalInstance, response) {
-            $scope.response = response;
+  return {
+    // Show a pretty modal with debug information about the error
+    showDetails: function (response) {
 
-            $scope.ok = function () {
-              window.location.reload();
-            };
-          }]
-        });
-      }
-    };
+      $modal.open({
+        templateUrl: 'views/modal_error.html',
+        resolve: {
+          response: function() {
+            return response;
+          }
+        },
+        controller: ['$scope', '$modalInstance', 'response', function($scope, $modalInstance, response) {
+          $scope.response = response;
 
-  });
+          $scope.ok = function () {
+            window.location.reload();
+          };
+        }]
+      });
+    }
+  };
+
+});
