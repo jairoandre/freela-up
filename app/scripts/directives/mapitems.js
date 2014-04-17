be62d3ae43e4258115c2738c6e83c08f5aa21f69
Gabriel Araujo <gabrielqa@me.com>
2014-04-17 04:10:06 -0700

    fixed link when viewing items

--- app/scripts/directives/mapitems.js
+++ app/scripts/directives/mapitems.js
@@ -239,7 +239,7 @@ angular.module('zupPainelApp')
               this.zoomLevels[this.map.getZoom()][type + '_' + item.id] = pin;
 
               google.maps.event.addListener(pin, 'click', function() {
-                var html = '<div class="pinTooltip"><h1>{{category.title}}</h1><p>Enviada {{ item.created_at | date: \'dd/MM/yy HH:mm\'}}</p><a href="#/inventories/{{ category.id }}/item/{{ item.id }}">Ver detalhes</a></div>';
+                var html = '<div class="pinTooltip"><h1>{{category.title}}</h1><p>Enviada {{ item.created_at | date: \'dd/MM/yy HH:mm\'}}</p><a href="#/inventories/categories/{{ category.id }}/item/{{ item.id }}">Ver detalhes</a></div>';
 
                 var new_scope = scope.$new(true);
 
