b4078ee933545f128f07f8023688e573753616b0
Gabriel Araujo <gabrielqa@me.com>
2014-04-25 07:24:42 -0700

    #ZUP-697 #done list categories

--- app/scripts/controllers/inventories.js
+++ app/scripts/controllers/inventories.js
@@ -182,8 +182,16 @@ angular.module('zupPainelApp')
       });
     };
   })
-  .controller('InventoriesCategoriesCtrl', function () {
+  .controller('InventoriesCategoriesCtrl', function ($scope, Restangular) {
+    $scope.loading = true;
 
+    var categoriesPromise = Restangular.one('inventory').all('categories').getList();
+
+    categoriesPromise.then(function(response) {
+      $scope.categories = response.data;
+
+      $scope.loading = false;
+    });
   })
   .controller('InventoriesCategoriesEditCtrl', function () {
 
