085602e315d3f583d936b67584456cb49787b5ed
Gabriel Araujo <gabrielqa@me.com>
2014-05-09 00:16:16 -0700

    refactoring onblur.js

--- app/scripts/directives/onblur.js
+++ app/scripts/directives/onblur.js
@@ -3,20 +3,20 @@
 angular.module('zupPainelApp')
   .directive('onBlur', function () {
     return {
-        restrict: 'A',
-        link: function(scope, elm, attrs) {
-            elm.bind('blur', function() {
-              if (typeof attrs.onBlurDelay !== 'undefined')
-              {
-                setTimeout(function() {
-                  scope.$apply(attrs.onBlur);
-                }, attrs.onBlurDelay);
-              }
-              else
-              {
-                scope.$apply(attrs.onBlur);
-              }
-            });
-        }
+      restrict: 'A',
+      link: function(scope, elm, attrs) {
+        elm.bind('blur', function() {
+          if (typeof attrs.onBlurDelay !== 'undefined')
+          {
+            setTimeout(function() {
+              scope.$apply(attrs.onBlur);
+            }, attrs.onBlurDelay);
+          }
+          else
+          {
+            scope.$apply(attrs.onBlur);
+          }
+        });
+      }
     };
   });
