920de5d7efb37be90b2a6ca5b047e1660131e329
Gabriel Araujo <gabrielqa@me.com>
2014-05-24 15:43:44 -0700

    added advanced search to inventory

--- app/scripts/controllers/inventories.js
+++ app/scripts/controllers/inventories.js
@@ -8,10 +8,15 @@ angular.module('zupPainelApp')
 
     $scope.loadingPagination = false;
 
-    $scope.selectedCategories = [];
-    $scope.selectedStatuses = [];
-    $scope.beginDate = null;
-    $scope.endDate = null;
+    // Basic filters
+    var resetFilters = function() {
+      $scope.selectedCategories = [];
+      $scope.selectedStatuses = [];
+      $scope.selectedUsers = [];
+      $scope.beginDate = null;
+      $scope.endDate = null;
+      $scope.searchText = null;
+    };
 
     // sorting the tables
     $scope.sort = {
@@ -33,20 +38,46 @@ angular.module('zupPainelApp')
       return column === $scope.sort.column && 'sort-' + $scope.sort.descending;
     };
 
+    // watch for filter type changes
+    $scope.$watchCollection('[advancedSearch, activeAdvancedFilters]', function() {
+      resetFilters();
+    });
+
+    $scope.availableFilters = [
+      {name: 'Com as categorias...', action: 'category'},
+      {name: 'Com os estados...', action: 'status'},
+      {name: 'Criado pelos munícipes...', action: 'author'},
+      {name: 'Por período...', action: 'date'}
+    ];
+
+    $scope.activeAdvancedFilters = [];
+
     // Return right promise
     var generateItemsPromise = function(searchText) {
       var url = Restangular.one('search').all('inventory').all('items'), options = { page: page, per_page: perPage }; // jshint ignore:line
 
       // if we searching, hit search/users
-      if (searchText !== '')
+      if ($scope.searchText !== null)
       {
-        options.query = searchText;
+        options.query = $scope.searchText;
       }
 
       // check if we have categories selected
       if ($scope.selectedCategories.length !== 0)
       {
         options.inventory_categories_ids = $scope.selectedCategories.join(); // jshint ignore:line
+      }
+
+      // check if we have statuses selected
+      if ($scope.selectedStatuses.length !== 0)
+      {
+        options.statuses_ids = $scope.selectedStatuses.join(); // jshint ignore:line
+      }
+
+      // check if we have statuses selected
+      if ($scope.selectedUsers.length !== 0)
+      {
+        options.users_ids = $scope.selectedUsers.join(); // jshint ignore:line
       }
 
       if ($scope.beginDate !== null)
@@ -142,6 +173,302 @@ angular.module('zupPainelApp')
     $scope.$watchCollection('[selectedCategories, selectedStatuses, beginDate, endDate]', function() {
       loadFilters();
     });
+
+    // We watch for changes in the advanced filter to set it's variables
+    $scope.$watch('advancedSearch', function() {
+      if ($scope.advancedSearch === true)
+      {
+        loadFilters();
+      }
+    });
+
+    $scope.$watch('activeAdvancedFilters', function() {
+      if ($scope.advancedSearch === true)
+      {
+        resetFilters();
+
+        for (var i = $scope.activeAdvancedFilters.length - 1; i >= 0; i--) {
+          var filter = $scope.activeAdvancedFilters[i];
+
+          if (filter.type === 'query')
+          {
+            $scope.searchText = filter.value;
+          }
+
+          if (filter.type === 'categories')
+          {
+            $scope.selectedCategories = filter.value;
+          }
+
+          if (filter.type === 'statuses')
+          {
+            $scope.selectedStatuses = filter.value;
+          }
+
+          if (filter.type === 'authors')
+          {
+            $scope.selectedUsers = filter.value;
+          }
+        }
+
+        loadFilters();
+      }
+    }, true);
+
+    $scope.removeFilter = function(filter) {
+      $scope.activeAdvancedFilters.splice($scope.activeAdvancedFilters.indexOf(filter), 1);
+    };
+
+    $scope.resetFilters = function() {
+      $scope.activeAdvancedFilters = [];
+    };
+
+    // All available filters
+    var advancedFilterQuery = function(query) {
+      var filter = {
+        title: 'Título ou endereço',
+        desc: query,
+        type: 'query',
+        value: query
+      };
+
+      $scope.activeAdvancedFilters.push(filter);
+    };
+
+    var advancedFilterCategory = function() {
+      $modal.open({
+        templateUrl: 'views/inventories/filters/category.html',
+        windowClass: 'filterCategoriesModal',
+        resolve: {
+          categories: function() {
+            return $scope.categories;
+          },
+
+          activeAdvancedFilters: function() {
+            return $scope.activeAdvancedFilters;
+          }
+        },
+        controller: ['$scope', '$modalInstance', 'categories', 'activeAdvancedFilters', function($scope, $modalInstance, categories, activeAdvancedFilters) {
+          $scope.categories = categories;
+          $scope.activeAdvancedFilters = activeAdvancedFilters;
+
+          $scope.updateCategory = function(category) {
+            var i = $scope.categories.indexOf(category);
+
+            if ($scope.categories[i].selected === true)
+            {
+              $scope.categories[i].selected = false;
+            }
+            else
+            {
+              $scope.categories[i].selected = true;
+            }
+          };
+
+          $scope.save = function() {
+            var filter = {
+              title: 'Categorias',
+              type: 'categories',
+              value: []
+            };
+
+            var desc = [];
+
+            for (var i = $scope.categories.length - 1; i >= 0; i--) {
+              if ($scope.categories[i].selected === true)
+              {
+                filter.value.push($scope.categories[i].id);
+                desc.push(' ' + $scope.categories[i].title);
+              }
+            }
+
+            filter.desc = desc.join();
+
+            $scope.activeAdvancedFilters.push(filter);
+
+            $modalInstance.close();
+          };
+
+          $scope.close = function() {
+            $modalInstance.close();
+          };
+        }]
+      });
+    };
+
+    var advancedFilterStatus = function() {
+      $modal.open({
+        templateUrl: 'views/inventories/filters/status.html',
+        windowClass: 'filterStatusesModal',
+        resolve: {
+          statuses: function() {
+            return $scope.statuses;
+          },
+
+          activeAdvancedFilters: function() {
+            return $scope.activeAdvancedFilters;
+          }
+        },
+        controller: ['$scope', '$modalInstance', 'statuses', 'activeAdvancedFilters', function($scope, $modalInstance, statuses, activeAdvancedFilters) {
+          $scope.statuses = statuses;
+          $scope.activeAdvancedFilters = activeAdvancedFilters;
+
+          $scope.updateStatus = function(status) {
+            var i = $scope.statuses.indexOf(status);
+
+            if ($scope.statuses[i].selected === true)
+            {
+              $scope.statuses[i].selected = false;
+            }
+            else
+            {
+              $scope.statuses[i].selected = true;
+            }
+          };
+
+          $scope.save = function() {
+            var filter = {
+              title: 'Estados',
+              type: 'statuses',
+              value: []
+            };
+
+            var desc = [];
+
+            for (var i = $scope.statuses.length - 1; i >= 0; i--) {
+              if ($scope.statuses[i].selected === true)
+              {
+                filter.value.push($scope.statuses[i].id);
+                desc.push(' ' + $scope.statuses[i].title);
+              }
+            }
+
+            filter.desc = desc.join();
+
+            $scope.activeAdvancedFilters.push(filter);
+
+            $modalInstance.close();
+          };
+
+          $scope.close = function() {
+            $modalInstance.close();
+          }; // hello
+        }]
+      });
+    };
+
+    var advancedFilterAuthor = function() {
+      $modal.open({
+        templateUrl: 'views/inventories/filters/author.html',
+        windowClass: 'filterAuthorModal',
+        resolve: {
+          activeAdvancedFilters: function() {
+            return $scope.activeAdvancedFilters;
+          }
+        },
+        controller: ['$scope', '$modalInstance', 'activeAdvancedFilters', function($scope, $modalInstance, activeAdvancedFilters) {
+          $scope.activeAdvancedFilters = activeAdvancedFilters;
+          $scope.users = [];
+          $scope.field = {};
+
+          $scope.usersAutocomplete = {
+            options: {
+              onlySelect: true,
+              source: function( request, uiResponse ) {
+                var categoriesPromise = Restangular.one('search').all('users').getList({ name: request.term });
+
+                categoriesPromise.then(function(response) {
+                  uiResponse( $.map( response.data, function( user ) {
+                    return {
+                      label: user.name,
+                      value: user.name,
+                      user: {id: user.id, name: user.name}
+                    };
+                  }));
+                });
+              },
+              messages: {
+                noResults: '',
+                results: function() {}
+              }
+            }
+          };
+
+          $scope.usersAutocomplete.events = {
+            select: function( event, ui ) {
+              var found = false;
+
+              for (var i = $scope.users.length - 1; i >= 0; i--) {
+                if ($scope.users[i].id === ui.item.user.id)
+                {
+                  found = true;
+                }
+              }
+
+              if (!found)
+              {
+                $scope.users.push(ui.item.user);
+              }
+            },
+
+            change: function() {
+              $scope.field.text = '';
+            }
+          };
+
+          $scope.removeUser = function(user) {
+            $scope.users.splice($scope.users.indexOf(user), 1);
+          };
+
+          $scope.save = function() {
+            var filter = {
+              title: 'Usuários',
+              type: 'authors',
+              value: []
+            };
+
+            var desc = [];
+
+            for (var i = $scope.users.length - 1; i >= 0; i--) {
+              filter.value.push($scope.users[i].id);
+              desc.push(' ' + $scope.users[i].name);
+            }
+
+            filter.desc = desc.join();
+
+            $scope.activeAdvancedFilters.push(filter);
+
+            $modalInstance.close();
+          };
+
+          $scope.close = function() {
+            $modalInstance.close();
+          };
+        }]
+      });
+    };
+
+    $scope.loadFilter = function(status) {
+      if (status === 'query')
+      {
+        advancedFilterQuery($scope.filterQuery);
+      }
+
+      if (status === 'category')
+      {
+        advancedFilterCategory();
+      }
+
+      if (status === 'status')
+      {
+        advancedFilterStatus();
+      }
+
+      if (status === 'author')
+      {
+        advancedFilterAuthor();
+      }
+    };
 
     // Search function
     $scope.search = function(text) {
