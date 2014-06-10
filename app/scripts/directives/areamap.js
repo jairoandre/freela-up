23aae4ad81bd89622464f73a9f2aa5072b80b1c2
Gabriel Araujo <gabrielqa@me.com>
2014-06-10 11:22:15 -0700

    #ZUP-666 added advanced filter for area in reports

---
+++ app/scripts/directives/areamap.js
@@ -0,0 +1,60 @@
+'use strict';
+
+angular.module('zupPainelApp')
+  .directive('areaMap', function () {
+    return {
+      restrict: 'A',
+      link: function postLink(scope, element, attrs) {
+        var mapProvider = {
+          options:
+          {
+            styles: [{}, {'featureType': 'poi.business', 'elementType': 'labels', 'stylers': [{ 'visibility': 'off' }] },{ 'featureType': 'poi.government', 'elementType': 'labels', 'stylers': [{ 'visibility': 'off' }] }, { 'featureType': 'poi.medical', 'elementType': 'labels', 'stylers': [{ 'visibility': 'off' }] }, { 'featureType': 'poi.place_of_worship', 'elementType': 'labels', 'stylers': [{ 'visibility': 'off' }] }, { 'featureType': 'poi.school', 'elementType': 'labels', 'stylers': [{ 'visibility': 'off' }] }, { 'featureType': 'poi.sports_complex', 'elementType': 'labels', 'stylers': [{ 'visibility': 'off' }] }, { 'featureType': 'transit', 'elementType': 'labels', 'stylers': [{ 'visibility': 'off' }, { 'saturation': -100 }, { 'lightness': 42 }] }, { 'featureType': 'road.highway', 'elementType': 'geometry.fill', 'stylers': [{ 'saturation': -100 }, { 'lightness': 47 }] }, { 'featureType': 'landscape', 'stylers': [{ 'lightness': 82 }, { 'saturation': -100 }] }, { 'featureType': 'water', 'stylers': [{ 'hue': '#00b2ff' }, { 'saturation': -21 }, { 'lightness': -4 }] }, { 'featureType': 'poi', 'stylers': [{ 'lightness': 19 }, { 'weight': 0.1 }, { 'saturation': -22 }] }, { 'elementType': 'geometry.fill', 'stylers': [{ 'visibility': 'on' }, { 'lightness': 18 }] }, { 'elementType': 'labels.text', 'stylers': [{ 'saturation': -100 }, { 'lightness': 28 }] }, { 'featureType': 'poi.attraction', 'elementType': 'labels', 'stylers': [{ 'visibility': 'off' }] }, { 'featureType': 'poi.park', 'elementType': 'geometry.fill', 'stylers': [{ 'saturation': 12 }, { 'lightness': 25 }] }, { 'featureType': 'road', 'elementType': 'labels.icon', 'stylers': [{ 'visibility': 'off' }] }, { 'featureType': 'road', 'elementType': 'labels.text', 'stylers': [{ 'lightness': 30 }] }, { 'featureType': 'landscape.man_made', 'elementType': 'labels', 'stylers': [{ 'visibility': 'off' }] }, { 'featureType': 'road.highway', 'elementType': 'geometry', 'stylers': [{ 'saturation': -100 }, { 'lightness': 56 }] }, { 'featureType': 'road.local', 'elementType': 'geometry.fill', 'stylers': [{ 'lightness': 62 }] }, { 'featureType': 'landscape.man_made', 'elementType': 'geometry', 'stylers': [{ 'visibility': 'off' }] }],
+            homeLatlng: new google.maps.LatLng(-23.549671, -46.6321713),
+            map: {
+              zoom: 11,
+              mapTypeControl: false,
+              panControl: true,
+              panControlOptions: {
+                position: google.maps.ControlPosition.TOP_RIGHT
+              },
+              zoomControl: true,
+              zoomControlOptions: {
+                position: google.maps.ControlPosition.TOP_RIGHT
+              },
+              scaleControl: true,
+              scaleControlOptions: {
+                position: google.maps.ControlPosition.TOP_RIGHT
+              },
+              streetViewControl: true,
+              streetViewControlOptions: {
+                position: google.maps.ControlPosition.TOP_RIGHT
+              }
+            }
+          },
+
+          start: function() {
+            // create map and set specific listeners
+            this.createMap();
+          },
+
+          createMap: function() {
+            var styledMap = new google.maps.StyledMapType(this.options.styles, { name: 'zup' });
+
+            this.map = new google.maps.Map(element[0], this.options.map);
+
+            this.map.mapTypes.set('zup', styledMap);
+            this.map.setMapTypeId('zup');
+            this.map.setCenter(this.options.homeLatlng);
+
+            setTimeout(function() {
+              google.maps.event.trigger(mapProvider.map, 'resize');
+              google.maps.event.trigger(mapProvider.map, 'bounds_changed');
+              mapProvider.map.setCenter(mapProvider.options.homeLatlng);
+            }, 1000);
+          },
+        };
+
+        mapProvider.start();
+      }
+    };
+  });
