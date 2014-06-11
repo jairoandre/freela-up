2bd30bab8f2bf17b5e67bd16306177379ec99161
Gabriel Araujo <gabrielqa@me.com>
2014-06-11 05:34:11 -0700

    #ZUP-839 fixed problem when changing value name

--- app/scripts/directives/inventorysinglevalue.js
+++ app/scripts/directives/inventorysinglevalue.js
@@ -5,6 +5,16 @@ angular.module('zupPainelApp')
     return {
       restrict: 'A',
       link: function postLink(scope) {
+        scope.editValue = angular.copy(scope.value);
+
+        scope.saveValue = function() {
+          scope.editingValue = false;
+
+          var index = scope.$parent.field.available_values.indexOf(scope.value);
+          scope.$parent.field.available_values[index] = scope.editValue;
+
+        };
+
         scope.removeValue = function(value) {
           var index = scope.$parent.field.available_values.indexOf(value); // jshint ignore:line
 
