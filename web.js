18dcc7312aca1d510979f686fc71103f4197743f
Gabriel Araujo <gabrielqa@me.com>
2014-06-16 17:20:25 -0700

    added index.html as /

--- web.js
+++ web.js
@@ -4,7 +4,13 @@ var gzippo = require('gzippo');
 var app = express();
 
 //app.use(express.logger());
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname);
 app.use(gzippo.staticGzip('' + __dirname));
+
+app.get('/', function (req, res) {
+  res.render('index.html');
+});
 
 var server = http.createServer(app);
 server.listen(process.env.PORT || 5000);
