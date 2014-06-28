cf8c50c94bff0c9e9080f9bde4f14c540469e122
Gabriel Araujo <gabrielqa@me.com>
2014-06-28 02:48:17 -0700

    #ZUP-858 fixed add flow modal

--- app/scripts/controllers/flows/flows.js
+++ app/scripts/controllers/flows/flows.js
@@ -17,7 +17,30 @@ angular.module('zupPainelApp')
   $scope.addFlow = function () {
     $modal.open({
       templateUrl: 'views/flows/add.html',
-      windowClass: 'addFlowModal'
+      windowClass: 'addFlowModal',
+      resolve: {
+        flows: function() {
+          return $scope.flows;
+        }
+      },
+      controller: ['$scope', '$modalInstance', 'flows', function($scope, $modalInstance, flows) {
+        $scope.flow = {};
+
+        $scope.save = function()
+        {
+          var postFlowPromise = Restangular.all('flows').post($scope.flow);
+
+          postFlowPromise.then(function(response) {
+            flows.push(Restangular.stripRestangular(response.data));
+
+            $modalInstance.close();
+          });
+        };
+
+        $scope.close = function() {
+          $modalInstance.close();
+        };
+      }]
     });
   };
 });
