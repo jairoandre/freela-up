6c6c3e403266f8afe4cd0d6a4010915890a740ea
Gabriel Araujo <gabrielqa@me.com>
2014-07-02 02:31:29 -0700

    #ZUP-862 added step field directive

---
+++ app/scripts/directives/flowstepfield.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('zupPainelApp')
+  .directive('flowStepField', function () {
+    return {
+      restrict: 'A',
+      link: function postLink(scope, element) {
+
+      }
+    };
+  });
