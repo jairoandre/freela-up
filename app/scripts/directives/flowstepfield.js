a1b8c7d48eec2ace8d835d5fd1debb41991915fd
Gabriel Araujo <gabrielqa@me.com>
2014-07-02 02:58:45 -0700

    #ZUP-862 update field title in the API

--- app/scripts/directives/flowstepfield.js
+++ app/scripts/directives/flowstepfield.js
@@ -6,6 +6,14 @@ angular.module('zupPainelApp')
       restrict: 'A',
       link: function postLink(scope, element) {
 
+        scope.saveTitle = function() {
+          var putFieldPromise = Restangular.one('flows', scope.flow.id).one('steps', scope.step.id).one('fields', scope.field.id).customPUT({title: scope.field.title});
+
+          putFieldPromise.then(function() {
+            scope.editingLabel = false;
+          });
+        };
+
       }
     };
   });
