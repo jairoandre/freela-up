1e8ba49cc6f6e45fb5df303a5c6eb75c2123b7ab
Gabriel Araujo <gabrielqa@me.com>
2014-07-27 22:48:22 -0300

    updated cases

--- app/scripts/controllers/cases/edit.js
+++ app/scripts/controllers/cases/edit.js
@@ -5,13 +5,29 @@ angular.module('zupPainelApp')
 .controller('CasesEditCtrl', function ($scope, Restangular, $modal, $routeParams) {
   var id = $routeParams.id;
 
+  $scope.loading = true;
+  $scope.currentTab = 'steps';
+
   var casePromise = Restangular.one('cases', id).get({'display_type': 'full'});
 
   casePromise.then(function(response) {
     $scope.case = response.data;
 
+    $scope.loading = false;
+
     console.log(Restangular.stripRestangular($scope.case));
   });
+
+  $scope.toggleTree = function() {
+    if ($scope.viewTree === true)
+    {
+      $scope.viewTree = false;
+    }
+    else
+    {
+      $scope.viewTree = true;
+    }
+  };
 
   $scope.selectConductor = function () {
     $modal.open({
