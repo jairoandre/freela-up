c42f07b1ddc1b10c8635f71cef445236fb3b8d65
Gabriel Araujo <gabrielqa@me.com>
2014-08-04 12:20:23 -0300

    #ZUP-946 change status inside item page

--- app/scripts/controllers/inventories/item.js
+++ app/scripts/controllers/inventories/item.js
@@ -12,6 +12,8 @@ angular.module('zupPainelApp')
     $scope.item = responses[0].data;
     $scope.category = responses[1].data;
 
+    console.log(Restangular.stripRestangular($scope.item));
+
     $scope.loading = false;
   });
 
@@ -24,6 +26,44 @@ angular.module('zupPainelApp')
     }
 
     return null;
+  };
+
+  $scope.editItemStatus = function (item, category) {
+    $modal.open({
+      templateUrl: 'views/inventories/items/editStatus.html',
+      windowClass: 'editStatusModal',
+      resolve: {
+        item: function() {
+          return item;
+        },
+
+        category: function() {
+          return category;
+        }
+      },
+      controller: ['$scope', '$modalInstance', 'category', 'item', function($scope, $modalInstance, category, item) {
+        $scope.category = category;
+        $scope.item = angular.copy(item);
+
+        $scope.changeStatus = function(statusId) {
+          $scope.item.inventory_status_id = statusId; // jshint ignore:line
+        };
+
+        $scope.save = function() {
+          var changeStatusPromise = Restangular.all('inventory').one('categories', $scope.category.id).one('items', $scope.item.id).customPUT({ 'inventory_status_id': $scope.item.inventory_status_id }); // jshint ignore:line
+
+          changeStatusPromise.then(function() {
+            item.inventory_status_id = $scope.item.inventory_status_id; // jshint ignore:line
+
+            $modalInstance.close();
+          });
+        };
+
+        $scope.close = function() {
+          $modalInstance.close();
+        };
+      }]
+    });
   };
 
   $scope.deleteItem = function (item, category) {
