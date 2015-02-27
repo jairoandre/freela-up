ebad3f38aac0938a4070d920f02b8b0e24761f17
Gabriel Araujo <gabrielqa@me.com>
2015-02-26 23:02:39 -0800

    date filter is now from the beggining of the 'from' date and the last of the 'to' date

--- app/modals/advanced-filters/period/advanced-filters-period.controller.js
+++ app/modals/advanced-filters/period/advanced-filters-period.controller.js
@@ -13,7 +13,7 @@ angular
           title: 'A partir da data',
           type: 'beginDate',
           desc: $scope.period.beginDate.getDate() + '/' + ($scope.period.beginDate.getMonth() + 1) + '/' + $scope.period.beginDate.getFullYear(),
-          value: moment($scope.period.beginDate).format()
+          value: moment($scope.period.beginDate).startOf('day').format()
         };
 
         $scope.activeAdvancedFilters.push(beginDateFilter);
@@ -25,7 +25,7 @@ angular
           title: 'Até a data',
           type: 'endDate',
           desc: $scope.period.endDate.getDate() + '/' + ($scope.period.endDate.getMonth() + 1) + '/' + $scope.period.endDate.getFullYear(),
-          value: moment($scope.period.endDate).format()
+          value: moment($scope.period.endDate).endOf('day').format()
         };
 
         $scope.activeAdvancedFilters.push(endDateFilter);
