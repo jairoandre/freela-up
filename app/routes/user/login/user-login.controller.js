79d3baf0cda780c6b70a899116a03e3406852a34
Gabriel Araujo <gabrielqa@me.com>
2015-03-16 18:23:22 -0700

    added loading to login page and blocked button (also added a little fading to error's message)

--- app/routes/user/login/user-login.controller.js
+++ app/routes/user/login/user-login.controller.js
@@ -4,11 +4,13 @@ angular
   .controller('UserLoginController', function($scope, $state, Auth) {
     $scope.login = function() {
       $scope.loginError = false;
+      $scope.processingRequest = true;
 
       Auth.login($scope.email, $scope.password).then(function() {
         $state.go('reports.list');
       }, function(response) {
         $scope.loginError = true;
+        $scope.processingRequest = false;
       });
     };
   });
