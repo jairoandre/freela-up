04903dc5aa20b4b5b0ca9886356992b933f4fb41
Gabriel Araujo <gabrielqa@me.com>
2015-04-10 23:39:17 -0700

    user can send message to report's author when updating status

--- app/modals/reports/edit-status/reports-edit-status.controller.js
+++ app/modals/reports/edit-status/reports-edit-status.controller.js
@@ -2,6 +2,7 @@
 
 angular
   .module('ReportsEditStatusModalControllerModule', [])
+
   .controller('ReportsEditStatusModalController', function(Restangular, $scope, $modalInstance, category, report) {
     $scope.category = category;
     $scope.report = angular.copy(report);
@@ -11,10 +12,18 @@ angular
     };
 
     $scope.save = function() {
-      var changeStatusPromise = Restangular.one('reports', $scope.category.id).one('items', $scope.report.id).customPUT({ 'status_id': $scope.report.status_id }); // jshint ignore:line
+      $scope.processing = true;
+
+      var visibility = 0;
+
+      if ($scope.report.privateComment) visibility = 1;
+
+      var changeStatusPromise = Restangular.one('reports', $scope.category.id).one('items', $scope.report.id).one('update_status').customPUT({ 'status_id': $scope.report.status_id, 'comment': $scope.report.comment, 'comment_visibility': visibility }); // jshint ignore:line
 
       changeStatusPromise.then(function() {
         report.status_id = $scope.report.status_id; // jshint ignore:line
+
+        $scope.processing = false;
 
         $modalInstance.close();
       });
