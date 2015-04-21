5d0a26fb8ceec7550cfbeb3f532d19e44cf2097e
Gabriel Araujo <gabrielqa@me.com>
2015-04-21 14:57:20 -0700

    fixed bugs with multiple-select-list

--- app/components/multiple-select-list/multiple-select-list.directive.js
+++ app/components/multiple-select-list/multiple-select-list.directive.js
@@ -3,15 +3,43 @@
 angular
   .module('MultipleSelectListComponentModule', [])
 
+  .directive('myTransclude', function() {
+    return {
+      compile: function(e, a, transclude) {
+        return function(scope, element) {
+          transclude(scope.$new(), function(clone) {
+            element.append(clone);
+          });
+        };
+      }
+    };
+  })
+
   .directive('multipleSelectList', function () {
     return {
         restrict: 'E',
         templateUrl: "components/multiple-select-list/multiple-select-list.template.html",
-        scope: {
-          ngModel : '=',
-        },
         transclude: true,
+        scope: {
+          ngModel: '='
+        },
+        replace: true,
         controller: function ($scope) {
+
+          $scope.selectAll = function(objects, key) {
+            for (var i = objects.length - 1; i >= 0; i--) {
+              var x = false;
+
+              for (var j = 0 ; j < $scope.ngModel.length; j++) {
+                if ($scope.ngModel[j] == objects[i][key])
+                {
+                  x = true;
+                }
+              };
+
+              if (!x) $scope.ngModel.push(objects[i][key]);
+            };
+          };
 
           if (!_.isArray($scope.ngModel)) $scope.ngModel = [];
 
@@ -23,11 +51,33 @@ angular
               break;
 
               case 1:
-                return '1 objeto selecionada';
+                return '1 objeto selecionado';
 
               default:
                 return $scope.ngModel.length + ' objetos selecionadas';
             }
+          };
+
+          $scope.toggle = function(option) {
+            var i = $scope.ngModel.indexOf(option);
+
+            if (~i)
+            {
+              $scope.ngModel.splice(i, 1);
+
+
+              return;
+            }
+
+            $scope.ngModel.push(option);
+          };
+
+          $scope.isSelected = function(option) {
+            var i = $scope.ngModel.indexOf(option);
+
+            if (!~i) return false;
+
+            return true;
           };
 
         }
