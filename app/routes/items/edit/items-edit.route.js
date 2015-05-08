8c7f03cfb13202025420d6d0e896d4aa43fc3551
Gabriel Araujo <gabrielqa@me.com>
2015-05-08 15:40:02 -0700

    #ZUP-2534 added return_fields to item's edit

--- app/routes/items/edit/items-edit.route.js
+++ app/routes/items/edit/items-edit.route.js
@@ -14,7 +14,15 @@ angular
           controllerAs: 'ctrl',
           resolve: {
             'categoryResponse': ['Restangular', '$stateParams', function(Restangular, $stateParams) {
-              return Restangular.one('inventory').one('categories', $stateParams.categoryId).get({display_type: 'full'});
+              var returnFields = [
+                'marker', 'pin', 'plot_format', 'require_item_status', 'statuses', 'title',
+                'sections.id', 'sections.title', 'sections.disabled', 'sections.required', 'sections.location', 'sections.position',
+                'sections.fields.id', 'sections.fields.disabled', 'sections.fields.title', 'sections.fields.kind', 'sections.fields.label',
+                'sections.fields.available_values', 'sections.fields.field_options', 'sections.fields.position', 'sections.fields.maximum',
+                'sections.fields.minimum', 'sections.fields.required'
+              ];
+
+              return Restangular.one('inventory').one('categories', $stateParams.categoryId).get({'display_type': 'full', 'return_fields': returnFields.join()});
             }],
 
             'itemResponse': ['Restangular', '$stateParams', function(Restangular, $stateParams) {
@@ -32,7 +40,15 @@ angular
           controllerAs: 'ctrl',
           resolve: {
             'categoryResponse': ['Restangular', '$stateParams', function(Restangular, $stateParams) {
-              return Restangular.one('inventory').one('categories', $stateParams.categoryId).get({display_type: 'full'});
+              var returnFields = [
+                'marker', 'pin', 'plot_format', 'require_item_status', 'statuses', 'title',
+                'sections.id', 'sections.title', 'sections.disabled', 'sections.required', 'sections.location', 'sections.position',
+                'sections.fields.id', 'sections.fields.disabled', 'sections.fields.title', 'sections.fields.kind', 'sections.fields.label',
+                'sections.fields.available_values', 'sections.fields.field_options', 'sections.fields.position', 'sections.fields.maximum',
+                'sections.fields.minimum', 'sections.fields.required'
+              ];
+
+              return Restangular.one('inventory').one('categories', $stateParams.categoryId).get({'display_type': 'full', 'return_fields': returnFields.join()});
             }],
 
             'itemResponse': function() {
