28d60f42e001bea27eba4c4ee87084caa5d91891
Nathan Ribeiro <lynxnathan@gmail.com>
2015-05-09 10:10:44 -0300

    ZUP-2611 Some kind of revert of this faith-based programming.

--- app/routes/items/edit/items-edit.route.js
+++ app/routes/items/edit/items-edit.route.js
@@ -1,56 +1,61 @@
 angular
-  .module('ItemsEditModule', [
-    'ItemsEditControllerModule'
-  ])
+    .module('ItemsEditModule', [
+      'ItemsEditControllerModule'
+    ])
 
-  .constant('categoryReturnFields', [
-    'id', 'marker', 'pin', 'plot_format', 'require_item_status', 'statuses', 'title',
-    'sections.id', 'sections.title', 'sections.disabled', 'sections.required', 'sections.location', 'sections.position',
-    'sections.fields.id', 'sections.fields.disabled', 'sections.fields.title', 'sections.fields.kind', 'sections.fields.label',
-    'sections.fields.available_values', 'sections.fields.field_options', 'sections.fields.position', 'sections.fields.maximum',
-    'sections.fields.minimum', 'sections.fields.required'
-  ])
+    .config(['$stateProvider', function($stateProvider) {
 
-  .config(['$stateProvider', function($stateProvider) {
+      $stateProvider.state('items.show.edit', {
+        url: '/category/{categoryId:[0-9]{1,9}}/edit',
+        views: {
+          '@': {
+            templateUrl: 'routes/items/edit/items-edit.template.html',
+            controller: 'ItemsEditController',
+            controllerAs: 'ctrl',
+            resolve: {
+              'categoryResponse': ['Restangular', '$stateParams', function(Restangular, $stateParams) {
+                var returnFields = [
+                  'id','marker', 'pin', 'plot_format', 'require_item_status', 'statuses', 'title',
+                  'sections.id', 'sections.title', 'sections.disabled', 'sections.required', 'sections.location', 'sections.position',
+                  'sections.fields.id', 'sections.fields.disabled', 'sections.fields.title', 'sections.fields.kind', 'sections.fields.label',
+                  'sections.fields.available_values', 'sections.fields.field_options', 'sections.fields.position', 'sections.fields.maximum',
+                  'sections.fields.minimum', 'sections.fields.required'
+                ];
 
-    $stateProvider.state('items.show.edit', {
-      url: '/category/{categoryId:[0-9]{1,9}}/edit',
-      views: {
-        '@': {
-          templateUrl: 'routes/items/edit/items-edit.template.html',
-          controller: 'ItemsEditController',
-          controllerAs: 'ctrl',
-          resolve: {
-            'categoryResponse': ['Restangular', '$stateParams', 'categoryReturnFields', function(Restangular, $stateParams, categoryReturnFields) {
-              return Restangular.one('inventory').one('categories', $stateParams.categoryId).get({'display_type': 'full', 'return_fields': categoryReturnFields.join()});
-            }],
+                return Restangular.one('inventory').one('categories', $stateParams.categoryId).get({'display_type': 'full', 'return_fields': returnFields.join()});
+              }],
 
-            'itemResponse': ['Restangular', '$stateParams', function(Restangular, $stateParams) {
-              var itemReturnFields = ['id', 'position', 'title', 'data.content', 'data.id', 'data.selected_options', 'field.id', 'locked', 'locker'];
-
-              return Restangular.one('inventory').one('items', $stateParams.id).get({ 'return_fields': itemReturnFields.join() });
-            }]
-          }
-        }
-      }
-    }).state('items.add', {
-      url: '/category/{categoryId:[0-9]{1,9}}/add',
-      views: {
-        '@': {
-          templateUrl: 'routes/items/edit/items-edit.template.html',
-          controller: 'ItemsEditController',
-          controllerAs: 'ctrl',
-          resolve: {
-            'categoryResponse': ['Restangular', '$stateParams', 'categoryReturnFields', function(Restangular, $stateParams, categoryReturnFields) {
-
-              return Restangular.one('inventory').one('categories', $stateParams.categoryId).get({'display_type': 'full', 'return_fields': categoryReturnFields.join()});
-            }],
-
-            'itemResponse': function() {
-              return false;
+              'itemResponse': ['Restangular', '$stateParams', function(Restangular, $stateParams) {
+                return Restangular.one('inventory').one('items', $stateParams.id).get();
+              }],
             }
           }
         }
-      }
-    });
-  }]);
+      }).state('items.add', {
+        url: '/category/{categoryId:[0-9]{1,9}}/add',
+        views: {
+          '@': {
+            templateUrl: 'routes/items/edit/items-edit.template.html',
+            controller: 'ItemsEditController',
+            controllerAs: 'ctrl',
+            resolve: {
+              'categoryResponse': ['Restangular', '$stateParams', function(Restangular, $stateParams) {
+                var returnFields = [
+                  'id','marker', 'pin', 'plot_format', 'require_item_status', 'statuses', 'title',
+                  'sections.id', 'sections.title', 'sections.disabled', 'sections.required', 'sections.location', 'sections.position',
+                  'sections.fields.id', 'sections.fields.disabled', 'sections.fields.title', 'sections.fields.kind', 'sections.fields.label',
+                  'sections.fields.available_values', 'sections.fields.field_options', 'sections.fields.position', 'sections.fields.maximum',
+                  'sections.fields.minimum', 'sections.fields.required'
+                ];
+
+                return Restangular.one('inventory').one('categories', $stateParams.categoryId).get({'display_type': 'full', 'return_fields': returnFields.join()});
+              }],
+
+              'itemResponse': function() {
+                return false;
+              }
+            }
+          }
+        }
+      });
+    }]);
