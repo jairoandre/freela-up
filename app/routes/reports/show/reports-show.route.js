6ae8b804492cdf35bc48ffed6da833acb7a8d640
Nathan Ribeiro <lynxnathan@gmail.com>
2015-05-12 13:44:34 -0300

    ZUP-2642 Relatos > Alteração de status e alteração do grupo responsável não leva em consideração ...

--- app/routes/reports/show/reports-show.route.js
+++ app/routes/reports/show/reports-show.route.js
@@ -11,11 +11,13 @@ angular
       resolve: {
         'reportResponse': ['Restangular', '$stateParams', function(Restangular, $stateParams) {
           var returnFields = [
-            'id', 'protocol', 'address', 'number', 'district', 'country', 'country', 'postal_code', 'state', 'city', 'created_at', 'description',
+            'id', 'protocol', 'address', 'number', 'district', 'country', 'country', 'postal_code', 'state', 'city', 'created_at', 'description', 'comment_required_when_updating_status',
             'feedback', 'images', 'inventory_item', 'inventory_item_category_id', 'overdue', 'position', 'reference', 'status',
             'user.name', 'user.email', 'user.id', 'user.phone',
             'assigned_user.id', 'assigned_user.name', 'assigned_group.id', 'assigned_group.name',
-            'category.id', 'category.marker', 'category.icon', 'category.solver_groups.id', 'category.solver_groups.name', 'category.solver_groups_ids', 'category.statuses', 'category.title', 'category.default_solver_group.name', 'category.default_solver_group.id'
+            'category.id', 'category.marker', 'category.icon', 'category.solver_groups.id',
+            'category.solver_groups.name', 'category.comment_required_when_updating_status', 'category.comment_required_when_forwarding',
+            'category.solver_groups_ids', 'category.statuses', 'category.title', 'category.default_solver_group.name', 'category.default_solver_group.id'
           ];
 
           return Restangular.one('reports').one('items', $stateParams.id).get({ 'return_fields': returnFields.join() });
