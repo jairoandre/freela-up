e1b9a8c57a9071468bd7fe77f1aab95677e09f09
Jairo <jairo.andre@gmail.com>
2015-07-01 22:28:55 -0300

    Incluido componente angular-promise-buttons ao painel. #ZUP-2989

--- app/modals/items/categories/destroy/items-categories-destroy.controller.js
+++ app/modals/items/categories/destroy/items-categories-destroy.controller.js
@@ -6,14 +6,12 @@ angular
   ])
   .controller('ItemsCategoriesDestroyModalController', function(Restangular, $scope, $modalInstance, inventoriesCategoriesList, category, $cookies, InventoriesCategoriesService) {
     $scope.category = category;
-    $scope.processing = false;
 
     // delete user from server
     $scope.confirm = function() {
-      $scope.processing = true;
-      var deletePromise = Restangular.one('inventory').one('categories', $scope.category.id).remove();
+      $scope.deletePromise = Restangular.one('inventory').one('categories', $scope.category.id).remove();
 
-      deletePromise.then(function() {
+      $scope.deletePromise.then(function() {
         delete $cookies.inventoryFiltersHash; // we need to remove the saved filters because the user might have selected the category
 
         // remove user from list
@@ -22,8 +20,6 @@ angular
         $scope.showMessage('ok', 'A categoria de inventário foi removida com sucesso.', 'success', true);
 
         InventoriesCategoriesService.purgeCache();
-
-        $scope.processing = false;
 
         $modalInstance.close();
       });
