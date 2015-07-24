c6eaa56844165c81a82e6e0f5d5cb0b0a3a5f7b3
Nathan Ribeiro <lynxnathan@gmail.com>
2015-07-24 14:16:44 -0300

    ZUP-3086 Added sanity check on timeout for focus-if component

--- app/components/focus-if/focus-if.directive.js
+++ app/components/focus-if/focus-if.directive.js
@@ -2,12 +2,14 @@
 
 angular.module('FocusIfComponentModule', [])
   .directive('focusIf', function ($timeout, $parse) {
+    var timeoutId;
     return {
       //scope: true,   // optionally create a child scope
       link: function (scope, element, attrs) {
         var ctx = $parse(attrs.focusIf);
         if (ctx.literal === true) {
-          $timeout(function () {
+          if(timeoutId) $timeout.cancel(timeoutId);
+          timeoutId = $timeout(function () {
             element[0].focus();
           }, 400);
         }
