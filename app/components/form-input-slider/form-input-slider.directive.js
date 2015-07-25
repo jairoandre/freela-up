9d06cccb02c6c125947f64082c81799d86258e14
Marcel Araujo <marcel.araujo@inphonex.com>
2015-07-25 00:07:24 -0300

    Fixes behavior when value is given by input. Sync the input field to input slider.

--- app/components/form-input-slider/form-input-slider.directive.js
+++ app/components/form-input-slider/form-input-slider.directive.js
@@ -4,7 +4,7 @@ angular.module('FormInputSliderComponentModule', [])
   .directive('formInputSlider', function () {
     return {
       restrict: 'CA',
-      require: 'ngModel',
+      require: '?ngModel',
       link: function (scope, element, attrs, ctrl) {
         if (!ctrl) {
           $log.warn('Slider directive requires ngModel to be on the element');
@@ -32,7 +32,6 @@ angular.module('FormInputSliderComponentModule', [])
         });
 
         ctrl.$render = function() {
-
           if (ctrl.$viewValue > 90) {
             el.noUiSlider.set(90);
           } else if (ctrl.$viewValue < 0) {
@@ -40,47 +39,18 @@ angular.module('FormInputSliderComponentModule', [])
           } else {
             el.noUiSlider.set(ctrl.$viewValue || 0);
           }
-
-          console.log('$render', ctrl);
         };
 
-        var value = 0;
-        el.noUiSlider.on('update', function(values, handle, unencoded) {
-          value = parseInt(values[0], 10) || 0;
+        var value;
+        el.noUiSlider.on('slide', function(values, handle, unencoded) {
+          value = parseInt(values[0] || 0, 10);
           scope.$evalAsync(update);
         });
         update();
 
         function update() {
-          //ctrl.$setViewValue(value);
-          console.log('update', value);
+          ctrl.$setViewValue(value);
         }
-
-        /*
-        // Specify how UI should be updated
-        ctrl.$render = function() {
-          element.html($sce.getTrustedHtml(ngModel.$viewValue || ''));
-        };
-
-        // Listen for change events to enable binding
-        element.on('blur keyup change', function() {
-          scope.$evalAsync(read);
-        });
-        read(); // initialize
-
-        // Write data to the model
-        function read() {
-          var html = element.html();
-          // When we clear the content editable the browser leaves a <br> behind
-          // If strip-br attribute is provided then we strip this out
-          if ( attrs.stripBr && html == '<br>' ) {
-            html = '';
-          }
-          ngModel.$setViewValue(html);
-        }
-        */
-
-        console.log(element);
       }
     };
   });
