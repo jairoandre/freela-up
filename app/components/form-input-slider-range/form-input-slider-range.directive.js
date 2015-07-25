c6dfbaec75e0d477bd4d871748436ca20bc9f9b4
Marcel Araujo <marcel.araujo@inphonex.com>
2015-07-25 03:12:41 -0300

    Fix slider range directive to support some rules. Fix slider styles. Fix notification filter template.

--- app/components/form-input-slider-range/form-input-slider-range.directive.js
+++ app/components/form-input-slider-range/form-input-slider-range.directive.js
@@ -1,27 +1,58 @@
 'use strict';
 
 angular.module('FormInputSliderRangeComponentModule', [])
-  .directive('formInputSliderRange', function () {
+  .directive('formInputSliderRange', function ($parse) {
     return {
       restrict: 'CA',
-      link: function(scope, element, attrs) {
+      require: '?ngModel',
+      link: function(scope, element, attrs, ctrl) {
+
+        var el = element[0];
 
         /**
          * Add the input-slider class
          */
-        angular.element( element ).addClass('input-slider-range');
+        angular.element(el).addClass('input-slider-range');
 
         /**
          * Create the single slider
          */
-        noUiSlider.create(element[0], {
-          start: 0,
+        noUiSlider.create(el, {
+          start: [0, 90],
           step: 1,
-          connect: 'lower',
+          connect: true,
           range: {
-            min: [ 0 ],
-            max: [ 90 ]
+            min: [0],
+            max: [90]
           }
+        });
+
+        var ngModelGet = $parse(attrs.ngModel);
+        scope.$watch(attrs.ngModel, function () {
+          var values = ngModelGet(scope);
+          var begin = parseInt(values.begin || 0, 10);
+          var end = parseInt(values.end || 90, 10);
+
+          if (end > 90) {
+            el.noUiSlider.set([null, 90]);
+          } else if (begin < 0) {
+            el.noUiSlider.set([0, null]);
+          } else {
+            el.noUiSlider.set([begin, end]);
+          }
+        }, true);
+
+        el.noUiSlider.on('slide', function(values, handle, unencoded) {
+          scope.$evalAsync(function() {
+            ctrl.$setViewValue({begin: unencoded[0], end: unencoded[1]});
+          });
+        });
+
+        ctrl.$parsers.push(function (value) {
+          return {
+            begin: value.begin || 0,
+            end: value.end || 90
+          };
         });
       }
     };
--- app/modals/advanced-filters/notification/overdue/advanced-filters-notification-overdue.controller.js
+++ app/modals/advanced-filters/notification/overdue/advanced-filters-notification-overdue.controller.js
@@ -1,13 +1,28 @@
 'use strict';
 
 angular
-  .module('AdvancedFiltersNotificationOverdueModalControllerModule', [])
+  .module('AdvancedFiltersNotificationOverdueModalControllerModule', [
+    'FormInputSliderRangeComponentModule'
+  ])
   .controller('AdvancedFiltersNotificationOverdueModalController', function(Restangular, $scope, $modalInstance, activeAdvancedFilters) {
-    $scope.activeAdvancedFilters = activeAdvancedFilters;
+    $scope.input = {
+      values: {
+        begin: 0,
+        end: 90
+      }
+    };
 
     $scope.save = function() {
-
-      $modalInstance.close();
+      //var filter = {
+      //  title: 'Última notificação emitida há',
+      //  desc: $scope.input.value + ( +($scope.input.value) > 1 ? " dias" : " dia"),
+      //  type: 'daysSinceLastNotification',
+      //  value: $scope.input.value
+      //};
+      //
+      //activeAdvancedFilters.push(filter);
+      //$modalInstance.close();
+      console.log($scope.input);
     };
 
     $scope.close = function() {
