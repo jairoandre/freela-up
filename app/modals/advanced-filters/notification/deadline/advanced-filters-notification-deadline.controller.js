d16ec56f15be0dfadf33c10c78a7a6c7cbb8b835
Marcel Araujo <marcel.araujo@inphonex.com>
2015-07-25 00:23:33 -0300

    Add new filter with fixes.

--- app/modals/advanced-filters/notification/deadline/advanced-filters-notification-deadline.controller.js
+++ app/modals/advanced-filters/notification/deadline/advanced-filters-notification-deadline.controller.js
@@ -3,10 +3,17 @@
 angular
   .module('AdvancedFiltersNotificationDeadlineModalControllerModule', [])
   .controller('AdvancedFiltersNotificationDeadlineModalController', function(Restangular, $scope, $modalInstance, activeAdvancedFilters) {
-    $scope.activeAdvancedFilters = activeAdvancedFilters;
+    $scope.input = {};
 
     $scope.save = function() {
+      var filter = {
+        title: 'Vencimento da última notificação',
+        desc: $scope.input.value + ( +($scope.input.value) > 1 ? " dias restantes" : " dia restante"),
+        type: 'daysForLastNotificationDeadline',
+        value: $scope.input.value
+      };
 
+      activeAdvancedFilters.push(filter);
       $modalInstance.close();
     };
 
