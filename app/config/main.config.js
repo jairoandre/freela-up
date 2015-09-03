18aec4f0614ad11c5c28a81a2673fe28a03ad61d
Marcel Araujo <marcel.araujo@inphonex.com>
2015-09-02 23:28:16 -0300

    Fixed z-index.

--- app/config/main.config.js
+++ app/config/main.config.js
@@ -88,12 +88,12 @@ angular
     $templateCache.put("template/modal/backdrop.html",
       "<div class=\"modal-backdrop fade {{ backdropClass }}\"\n" +
       "     ng-class=\"{in: animate}\"\n" +
-      "     ng-style=\"{'z-index': 1040 + (index && 1 || 0) + index*10}\"\n" +
+      "     ng-style=\"{'z-index': 10040 + (index && 1 || 0) + index*10}\"\n" +
       "></div>\n" +
       "");
 
     $templateCache.put("template/modal/window.html",
-      "<div tabindex=\"-1\" role=\"dialog\" class=\"modal fade\" ng-class=\"{in: animate}\" ng-style=\"{'z-index': 1050 + index*10, display: 'block'}\" ng-click=\"close($event)\">\n" +
+      "<div tabindex=\"-1\" role=\"dialog\" class=\"modal fade\" ng-class=\"{in: animate}\" ng-style=\"{'z-index': 10050 + index*10, display: 'block'}\" ng-click=\"close($event)\">\n" +
       "    <div class=\"modal-dialog\" ng-class=\"{'modal-sm': size == 'sm', 'modal-lg': size == 'lg'}\"><div class=\"modal-content\" modal-transclude></div></div>\n" +
       "</div>");
 
