52a0e7e13355d4c3c861cf1eb0c94c9f8c76acab
Jairo Oliveira <Jairo Oliveira>
2015-10-29 16:15:28 -0200

    Fix google maps filter to neighborhood

--- app/helpers/get-google-address-components/get-google-address-components.filter.js
+++ app/helpers/get-google-address-components/get-google-address-components.filter.js
@@ -13,6 +13,8 @@ angular
         _.each(c.types, function(type) {
           if (type === 'route') treatedComponents['address'] = c.long_name
           else if (type === 'neighborhood') treatedComponents['neighborhood'] = c.long_name
+          else if (type === 'sublocality_level_1') treatedComponents['neighborhood'] = c.long_name
+          else if (type === 'sublocality') treatedComponents['neighborhood'] = c.long_name
           else if (type === 'locality') treatedComponents['city'] = c.long_name
           else if (type === 'administrative_area_level_1') treatedComponents['state'] = c.short_name
           else if (type === 'country') treatedComponents['country'] = c.long_name;
