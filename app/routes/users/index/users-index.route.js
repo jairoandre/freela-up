0ca249c2510028299e8bdb0bd6d86f469c197a8b
David Guilherme <davidlguilherme@gmail.com>
2016-01-07 10:55:33 -0300

    Redirect users unauthorized to inventory page

--- app/routes/users/index/users-index.route.js
+++ app/routes/users/index/users-index.route.js
@@ -8,17 +8,6 @@ angular
     $stateProvider.state('users.list', {
       url: '',
       resolve: {
-        'Authorize': function ($rootScope, $route) {
-          if (!$rootScope.hasAnyPermission(['users_full_access', 'users_edit')) {
-            $rootScope.showMessage(
-              'exclamation-sign',
-              'Você não possui permissão para visualizar essa página.',
-              'error'
-            );
-
-            $route.go('items.list');
-          }
-        },
         'groupsResponse': ['Restangular', function(Restangular) {
           return Restangular.all('groups').getList({ 'return_fields': 'id,name' });
         }]
--- app/routes/users/users.route.js
+++ app/routes/users/users.route.js
@@ -14,7 +14,18 @@ angular
       resolve: {
         'User': ['User', function(User) {
           return User({ permissions: ['isLogged'] });
-        }]
+        }],
+        'Authorize': function ($rootScope, $state, User) {
+          if (!$rootScope.hasAnyPermission(['users_full_access', 'users_edit'])) {
+            $rootScope.showMessage(
+              'exclamation-sign',
+              'Você não possui permissão para visualizar essa página.',
+              'error'
+            );
+
+            $state.go('items.list');
+          }
+        }
       }
     });
 
